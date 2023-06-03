9dd590cadbab3095ce720f7161594c9fad0d4151
Sergey Fedorov <fedyaser@inbox.ru>
2023-06-03 18:49:01 +0700

    update test whith each

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -1,21 +1,11 @@
 import getHealthStatus from '../app';
 
-describe('getHealthStatus', () => {
-  test('Здоровье более 50 - зелёный "healthy"', () => {
-    const character = { name: 'Маг', health: 90 };
-    const result = getHealthStatus(character);
-    expect(result).toBe('healthy');
-  });
-
-  test('Здоровье от 50 и до 15 - жёлтый "wounded" ', () => {
-    const character = { name: 'Маг', health: 30 };
-    const result = getHealthStatus(character);
-    expect(result).toBe('wounded');
-  });
-
-  test('Менее 15 - красный "critical" ', () => {
-    const character = { name: 'Маг', health: 10 };
-    const result = getHealthStatus(character);
-    expect(result).toBe('critical');
-  });
+test.each([
+  ['Маг', 90, 'healthy'],
+  ['Маг', 30, 'wounded'],
+  ['Маг', 10, 'critical'],
+])('test func whith %s person, %h - health, %c color', (person, health, stat) => {
+  const character = { name: person, health };
+  const result = getHealthStatus(character);
+  expect(result).toBe(stat);
 });
