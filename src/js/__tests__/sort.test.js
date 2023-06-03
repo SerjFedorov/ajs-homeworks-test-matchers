78f68afefef3397538ec2c0dec9cf89e2d7ff66e
Sergey Fedorov <fedyaser@inbox.ru>
2023-06-03 18:21:22 +0700

    Task compl

---
+++ src/js/__tests__/sort.test.js
@@ -0,0 +1,19 @@
+import sortHeroes from '../sort';
+
+describe('sortHeroes', () => {
+  test('should sort heroes by health in descending order', () => {
+    const heroes = [
+      { name: 'мечник', health: 10 },
+      { name: 'маг', health: 100 },
+      { name: 'лучник', health: 80 },
+    ];
+
+    const sortedHeroes = sortHeroes(heroes);
+
+    expect(sortedHeroes).toEqual([
+      { name: 'маг', health: 100 },
+      { name: 'лучник', health: 80 },
+      { name: 'мечник', health: 10 },
+    ]);
+  });
+});
